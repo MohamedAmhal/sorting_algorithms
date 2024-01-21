77c709c1e01ca2a3ced93a4c16fe8591d28078ba
MohamedAmhal <mohammedamhal349@gmail.com>
2024-01-21 03:46:22 -0800

    up

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -15,6 +15,10 @@ void bubble_sort(int *array, size_t size)
 	size_t i;
 	size_t j;
 
+	if (array == NULL || size < 2)
+	{
+		return;
+	}
 	for (i = 0; i < size - 1; i++)
 	{
 	for (j = 0; j < size - 1 - i; j++)
